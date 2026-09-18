86279aa678176780a6fb501c5a507b07131dfb8a
Quinha16 <jessicaraissa2017@gmail.com>
2026-09-18 19:49:27 -0300

    Update escala.py

--- escala.py
+++ escala.py
@@ -7,7 +7,7 @@ def cadastrar_escalas():
     escala = input("Digite o nome da Escala: ")
     with open(path_bd,"a", encoding="utf-8") as arquivo:
         arquivo.write(f"{escala}\n")
-    escalas.append(escala)
+
     print(f"O nome cadastrado foi: {escala}")
     print("===================================================")
     print("Você gostaria de adicionar uma nova Escala?")
@@ -23,8 +23,12 @@ def cadastrar_escalas():
 def listar_escala(): 
     with open(path_bd,"r", encoding="utf-8") as arquivo:
         for linha in arquivo:
-            print(linha.strip())
-            
+            nome_limpo = linha.strip()
+            if nome_limpo not in escalas:
+                escalas.append(nome_limpo)
+
+    print("\n".join(escalas))
+    
 def excluir_escala():
     listar_escala()
     escala = input("Qual Escala você deseja deletar?: ")
